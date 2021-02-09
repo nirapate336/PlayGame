7e57183df756ed465bdecffdc17e72722e605dea
Nirav Patel <nirapate336@outlook.com>
2021-02-09 09:55:05 -0500

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.DimGray;
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.startButton.FlatAppearance.BorderSize = 4;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.PaleTurquoise;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tomato;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("mono 07_66", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.White;
+            this.startButton.Location = new System.Drawing.Point(201, 108);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(296, 118);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Begin Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.AutoSize = true;
+            this.countdownLabel.Font = new System.Drawing.Font("mono 07_65", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.White;
+            this.countdownLabel.Location = new System.Drawing.Point(178, 238);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(0, 31);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(675, 376);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Arcade Central";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            //playing the sound
+            SoundPlayer bellPlayer = new SoundPlayer(Properties.Resources.Door_Bell_SoundBible_com_1986366504);
+            bellPlayer.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //get rid of start button
+            startButton.Visible = false;
+
+            //countdown 3 label and sound
+            countdownLabel.Text = "Game will begin in 3...";
+            SoundPlayer countdown1Player = new SoundPlayer(Properties.Resources.Door_Bell_SoundBible_com_1986366504);
+            countdown1Player.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+           //countdown 2 label and sound
+            countdownLabel.Text = "Game will begin in 2...";
+            SoundPlayer countdown2Player = new SoundPlayer(Properties.Resources.Door_Bell_SoundBible_com_1986366504);
+            countdown2Player.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //countdown 1 Label and sound
+            countdownLabel.Text = "Game will begin in 1...";
+            SoundPlayer countdown3Player = new SoundPlayer(Properties.Resources.Door_Bell_SoundBible_com_1986366504);
+            countdown3Player.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            //start and backround colour
+            countdownLabel.Text = "STARTING GAME !!!";
+            this.BackColor = Color.SpringGreen; 
+            SoundPlayer startPlayer = new SoundPlayer(Properties.Resources.Boxing_Bell_Start_Round_SoundBible_com_1691615580);
+            startPlayer.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+
+
+
+        }
+    }
+}
